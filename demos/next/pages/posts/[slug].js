c5dd3be975dd50b1e9e136ef6ccadc07cc25c6fa
maggie-j-liu <maggie.j.liu@gmail.com>
2021-08-06 22:18:51 -0700

    fix?

--- demos/next/pages/posts/[slug].js
+++ demos/next/pages/posts/[slug].js
@@ -19,17 +19,20 @@ export default function Post({ post, morePosts, preview }) {
   }
 
   useEffect(() => {
-    reactive.install({
-      firebaseConfig: {
-        apiKey: "AIzaSyAux7Beoq9hQiT4TgQGnyNWR3_BwTukmaU",
-        authDomain: "reactive-db9e1.firebaseapp.com",
-        databaseURL: "https://reactive-db9e1-default-rtdb.firebaseio.com",
-        projectId: "reactive-db9e1",
-        storageBucket: "reactive-db9e1.appspot.com",
-        messagingSenderId: "248078740379",
-        appId: "1:248078740379:web:bebe06abacef70e4b50f3e",
-      },
-    });
+    console.log("useeffect");
+    if (typeof reactive !== "undefined") {
+      reactive.install({
+        firebaseConfig: {
+          apiKey: "AIzaSyAux7Beoq9hQiT4TgQGnyNWR3_BwTukmaU",
+          authDomain: "reactive-db9e1.firebaseapp.com",
+          databaseURL: "https://reactive-db9e1-default-rtdb.firebaseio.com",
+          projectId: "reactive-db9e1",
+          storageBucket: "reactive-db9e1.appspot.com",
+          messagingSenderId: "248078740379",
+          appId: "1:248078740379:web:bebe06abacef70e4b50f3e",
+        },
+      });
+    }
   }, []);
 
   return (
