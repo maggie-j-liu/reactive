0ed98db56ebc21ae02dc38960867ccabe62c4d6a
maggie-j-liu <maggie.j.liu@gmail.com>
2021-08-13 09:27:11 -0700

    dark mode markdown

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,27 @@ module.exports = {
         dark: {
           css: {
             color: theme("colors.white"),
+            h1: {
+              color: "inherit",
+            },
+            h2: {
+              color: "inherit",
+            },
+            h3: {
+              color: "inherit",
+            },
+            h4: {
+              color: "inherit",
+            },
+            h5: {
+              color: "inherit",
+            },
+            h6: {
+              color: "inherit",
+            },
+            a: {
+              color: theme("colors.primary.300"),
+            },
             blockquote: {
               color: "inherit",
             },
