df4115e142c1dbb46e8a7e68051a568260f4cb04
Dennis Chen <proofprogram@gmail.com>
2021-09-06 17:08:23 -0700

    Hard removed x scroll: should not pop up on chrome w/ weird zooming anymore

--- src/components/Comments.svelte
+++ src/components/Comments.svelte
@@ -184,11 +184,11 @@
 
 <div class="r-flex r-flex-col r-gap-4 r-mt-16 r-text-base r-w-full">
   {#if previewMode}
-    <p class="r-text-gray-600 dark:!r-text-gray-200">
+    <p class="r-text-gray-600 dark:!r-text-gray-200 r-overflow-x-hidden r-break-words">
       Add a comment (markdown is supported)
     </p>
     <div
-      class="r-max-h-96 r-scroll-auto r-text-base r-prose dark:!r-prose-dark r-max-w-none dark:!r-text-white r-bg-gray-100 dark:!r-bg-gray-700 r-px-6 r-py-4"
+      class="r-max-h-96 r-overflow-x-hidden r-overflow-y-auto r-break-words r-text-base r-prose dark:!r-prose-dark r-max-w-none dark:!r-text-white r-bg-gray-100 dark:!r-bg-gray-700 r-px-6 r-py-4"
     >
       <SvelteMarkdown source={currentComment || "Nothing to preview"} />
     </div>
@@ -264,7 +264,7 @@
               </span>
             </h4>
             <div
-              class="r-max-h-96 r-overflow-y-auto r-break-words r-text-base r-prose dark:!r-prose-dark r-max-w-none dark:!r-text-white"
+              class="r-max-h-96 r-overflow-y-auto r-overflow-x-hidden r-break-words r-text-base r-prose dark:!r-prose-dark r-max-w-none dark:!r-text-white"
             >
               <SvelteMarkdown source={comment.text} />
             </div>
